b131fb156d9c63863f9da77ef57c3111695f4bcb
khouloudRais <khouloud.kechichhe-rais@laplateforme.io>
2025-02-10 23:21:09 +0100

    créé le projet QUIZ et la page d'acceuil de ce projet

---
+++ acceuil/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page d'acceuil</title>
+    <link rel="stylesheet" href="acceuil.css">
+</head>
+<body>
+<header>
+    <h1> Quiz & Découverte!</h1>
+</header>
+
+    
+<nav>
+  <ul>
+    <li><a href="#accueil">Accueil</a></li>
+    <li><a href="#apropos">À propos</a></li>
+    <li><a href="#services">Services</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#connexion" class="btn-connexion">Connexion</a></li>
+  </ul>
+</nav>
+
+
+<div class="main-container">
+   
+    <div class="section">
+      <h2>Culture générale</h2>
+      <p>Testez vos connaissances en culture générale avec cette catégorie.</p>
+      <a href="#" class="btn">Commencer</a>
+    </div>
+
+   
+    <div class="section">
+      <h2>Sport</h2>
+      <p>Défiez-vous dans cette catégorie de sport</p>
+      <a href="#" class="btn">Commencer</a>
+    </div>
+
+    <div class="section">
+      <h2>histoire</h2>
+      <p>Testez vos connaissances en histoire</p>
+      <a href="#" class="btn">Commencer</a>
+    </div>
+  </div>
+
+
+</body>
+<footer>
+  <p>&copy; 2025 Quiz & Découverte. Tous droits réservés.</p>
+
+</footer>
+
+</html>
