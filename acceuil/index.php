e9a6adb6be2a23f9df8fcc1d5512e911043d1fdc
khouloudRais <khouloud.kechichhe-rais@laplateforme.io>
2025-02-12 10:07:06 +0100

    mattre a jour page d'acceuil

--- acceuil/index.php
+++ acceuil/index.php
@@ -1,55 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>page d'acceuil</title>
+    <title>Page d'accueil</title>
     <link rel="stylesheet" href="acceuil.css">
 </head>
 <body>
-<header>
-    <h1> Quiz & Découverte!</h1>
-</header>
-
+    <header>
+        <h1>Quiz & Découverte!</h1>
+    </header>
     
-<nav>
-  <ul>
-    <li><a href="#accueil">Accueil</a></li>
-    <li><a href="#apropos">À propos</a></li>
-    <li><a href="#services">Services</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li><a href="#connexion" class="btn-connexion">Connexion</a></li>
-  </ul>
-</nav>
+    <nav>
+        <ul>
+            <li><a href="#accueil">Accueil</a></li>
+            <li><a href="#apropos">À propos</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#connexion" class="btn-connexion">Connexion</a></li>
+        </ul>
+    </nav>
 
+    <div class="main-container">
+        <div class="section">
+        <img src="image/culture.webp" alt="Culture générale" class="section-image">
+            <h2>Culture générale</h2>
+            <p>Testez vos connaissances en culture générale avec cette catégorie.</p>
+            <a href="culture.php" class="btn">Commencer</a>
+            
 
-<div class="main-container">
-   
-    <div class="section">
-      <h2>Culture générale</h2>
-      <p>Testez vos connaissances en culture générale avec cette catégorie.</p>
-      <a href="#" class="btn">Commencer</a>
+        </div>
+
+        <div class="section">
+        <img src="image/sport.jpeg" alt="Sport" class="section-image">
+            <h2>Sport</h2>
+            <p>Défiez-vous dans cette catégorie de sport</p>
+            <a href="sport.php" class="btn">Commencer</a>
+           
+        </div>
+
+        <div class="section">
+        <img src="image/histoire.jpg" alt="Histoire" class="section-image">
+            <h2>Histoire</h2>
+            <p>Testez vos connaissances en histoire</p>
+            <a href="histoire.php" class="btn">Commencer</a>
+          
+        </div>
     </div>
 
-   
-    <div class="section">
-      <h2>Sport</h2>
-      <p>Défiez-vous dans cette catégorie de sport</p>
-      <a href="#" class="btn">Commencer</a>
-    </div>
-
-    <div class="section">
-      <h2>histoire</h2>
-      <p>Testez vos connaissances en histoire</p>
-      <a href="#" class="btn">Commencer</a>
-    </div>
-  </div>
-
-
+    <footer>
+        <p>&copy; 2025 Quiz & Découverte. Tous droits réservés.</p>
+    </footer>
 </body>
-<footer>
-  <p>&copy; 2025 Quiz & Découverte. Tous droits réservés.</p>
-
-</footer>
-
 </html>
